bb6b4a691ed8283227b9295bc530bf0f3e554f73
Vishal Dubey <yzvishal.vd@gmail.com>
2020-02-21 00:41:34 +0530

    handle parsing of multiple timezones

--- lib/common/NowTz.dart
+++ lib/common/NowTz.dart
@@ -20,14 +20,19 @@ class _NowTzState extends State<NowTz> {
   Location validLoc;
   Timer timer;
 
+  int selectedIndex = 0;
+
+  List<dynamic> timezonesList = [];
+  var tz = '';
+
   @override
   void initState() {
     var tzType = widget.tzData['type'];
-    var tz = widget.tzData['tz'];
+    tz = widget.tzData['tz'];
     if (tzType == 'tz') {
-      var filteredLocation =
-          timezones.firstWhere((timezone) => timezone['abbr'] == tz);
-      validLoc = getValidLocation(filteredLocation['utc']);
+      timezonesList =
+          timezones.where((timezone) => timezone['abbr'] == tz).toList();
+      validLoc = getValidLocation(timezonesList[0]['utc']);
       location = validLoc.name;
     } else {
       try {
@@ -46,6 +51,15 @@ class _NowTzState extends State<NowTz> {
     super.initState();
   }
 
+  onTimezoneChange(index) {
+    validLoc = getValidLocation(timezonesList[index]['utc']);
+    setState(() {
+      selectedIndex = index;
+      location = validLoc.name;
+      convertedDate = TZDateTime.from(DateTime.now(), validLoc);
+    });
+  }
+
   Location getValidLocation(List<String> locations) {
     String validLocation = locations.firstWhere((location) {
       try {
@@ -61,47 +75,115 @@ class _NowTzState extends State<NowTz> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         title: Text('Time in $location'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(16, 24, 16, 16),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            const Text(
-              'The time right now',
-              textAlign: TextAlign.center,
-              style: const TextStyle(fontSize: 22),
-            ),
-            SizedBox(
-              height: 16,
-            ),
-            Text(
-              '${DateFormat('hh:mm:ss a').format(convertedDate)}',
-              textAlign: TextAlign.center,
-              style: const TextStyle(
-                  fontFamily: 'Pixel', fontSize: 44, color: Color(0xff373e9e)),
-            ),
-            SizedBox(
-              height: 16,
-            ),
-            const Text(
-              'in the timezone',
-              textAlign: TextAlign.center,
-              style: const TextStyle(fontSize: 22),
-            ),
-            SizedBox(
-              height: 8,
-            ),
-            Text(
-              '$location',
-              textAlign: TextAlign.center,
-              style: const TextStyle(
-                  fontFamily: 'Pixel', fontSize: 30, color: Colors.deepPurple),
-            ),
-          ],
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(16, 32, 16, 16),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              timezonesList.length > 1 ? Padding(
+                padding: const EdgeInsets.only(bottom: 32),
+                child: RichText(
+                  text: TextSpan(
+                    children: [
+                      TextSpan(
+                        text: 'Uh oh! ',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontFamily: 'CabinSketch',
+                          fontSize: 20
+                        )
+                      ),
+                      TextSpan(
+                        text: '$tz ',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w700,
+                            color: Colors.black,
+                            fontFamily: 'CabinSketch',
+                            fontSize: 20
+                        )
+                      ),
+                      TextSpan(
+                        text: 'means more than one timezone\n\nWhat it might stand for?',
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontFamily: 'CabinSketch',
+                            fontSize: 20
+                        )
+                      )
+                    ]
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ): Text(''),
+              timezonesList.length > 1
+                  ? Flexible(
+                      child: GridView.builder(
+                          shrinkWrap: true,
+                          gridDelegate:
+                              SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 2,
+                          ),
+                          itemCount: timezonesList.length,
+                          itemBuilder: (context, position) {
+                            return timeZoneCard(timezonesList[position],
+                                position, selectedIndex, onTimezoneChange);
+                          }),
+                    )
+                  : Container(
+                      width: 0,
+                      height: 0,
+                    ),
+              timezonesList.length > 1
+                  ? SizedBox(
+                      height: 24,
+                    )
+                  : SizedBox(
+                      height: 0,
+                    ),
+              const Text(
+                'The time right now',
+                textAlign: TextAlign.center,
+                style: const TextStyle(fontSize: 22),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Text(
+                '${DateFormat('hh:mm:ss a').format(convertedDate)}',
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                    fontFamily: 'Pixel',
+                    fontSize: 44,
+                    color: Color(0xff373e9e)),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              const Text(
+                'in the timezone',
+                textAlign: TextAlign.center,
+                style: const TextStyle(fontSize: 22),
+              ),
+              SizedBox(
+                height: 8,
+              ),
+              Text(
+                '$location',
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                    fontFamily: 'Pixel',
+                    fontSize: 30,
+                    color: Colors.deepPurple),
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -111,5 +193,42 @@ class _NowTzState extends State<NowTz> {
   void dispose() {
     timer.cancel();
     super.dispose();
+  }
+
+  Widget timeZoneCard(dynamic data, index, selectedIndex, onTimezoneChange) {
+    return InkWell(
+      onTap: () {
+        onTimezoneChange(index);
+      },
+      child: Card(
+        elevation: 4,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16),
+          side: index == selectedIndex
+              ? BorderSide(color: Colors.deepPurple, width: 2)
+              : BorderSide.none,
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: Text(
+                data['value'],
+                style: TextStyle(
+                    color: Theme.of(context).accentColor, fontSize: 22),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(16, 0, 16, 16),
+              child: Text(
+                data['text'],
+                style: TextStyle(color: Colors.grey, fontSize: 16),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/common/ShowParsedTz.dart
+++ lib/common/ShowParsedTz.dart
@@ -15,18 +15,24 @@ class ShowParsedTz extends StatefulWidget {
 
 class _ShowParsedTzState extends State<ShowParsedTz> {
   String parsedTime, location, offset, convertedTime, userTz;
-  var filteredLocation;
   Location validLoc;
   var userParsedTime;
+  List<dynamic> timezonesList = [];
+  var tz = '';
+  var userTzName = '';
+  int selectedTzIndex = 0;
+  int selectedUserTzIndex = 0;
+
+  List<dynamic> userTzList = [];
 
   @override
   void initState() {
     parsedTime = widget.data['time'];
-    var tz = widget.data['tz'];
+    tz = widget.data['tz'];
     if (widget.data['type'] == 'tz') {
-      filteredLocation =
-          timezones.firstWhere((timezone) => timezone['abbr'] == tz);
-      validLoc = getValidLocation(filteredLocation['utc']);
+     timezonesList =
+          timezones.where((timezone) => timezone['abbr'] == tz).toList();
+      validLoc = getValidLocation(timezonesList[0]['utc']);
       location = validLoc.name;
     } else {
       try {
@@ -35,6 +41,7 @@ class _ShowParsedTzState extends State<ShowParsedTz> {
       } catch (error) {}
     }
     var currentTime = DateTime.now();
+    userTzName = currentTime.timeZoneName;
     var tzParsedTime = TZDateTime(
         validLoc,
         currentTime.year,
@@ -42,14 +49,56 @@ class _ShowParsedTzState extends State<ShowParsedTz> {
         currentTime.day,
         int.parse(parsedTime.substring(0, 2)),
         int.parse(parsedTime.substring(2, 4)));
-    var userTZData = timezones.firstWhere((timezone) =>
-    timezone['abbr'] == currentTime.timeZoneName)['utc'];
+    userTzList = timezones.where((timezone) =>
+    timezone['abbr'] == userTzName).toList();
     userParsedTime = TZDateTime.from(
         tzParsedTime,
-        getValidLocation(userTZData));
-    userTz = userTZData[0];
+        getValidLocation(userTzList[0]['utc']));
+    userTz = userTzList[0]['utc'][0];
     convertedTime = DateFormat('hh:mm:ss a').format(userParsedTime);
     super.initState();
+  }
+
+  onTimezoneChange(index) {
+    validLoc = getValidLocation(timezonesList[index]['utc']);
+    var currentTime = DateTime.now();
+    var tzParsedTime = TZDateTime(
+        validLoc,
+        currentTime.year,
+        currentTime.month,
+        currentTime.day,
+        int.parse(parsedTime.substring(0, 2)),
+        int.parse(parsedTime.substring(2, 4)));
+    userParsedTime = TZDateTime.from(
+        tzParsedTime,
+        getValidLocation(userTzList[selectedUserTzIndex]['utc']));
+    setState(() {
+      selectedTzIndex = index;
+      location = validLoc.name;
+      userTz = userTzList[selectedUserTzIndex]['utc'][0];
+      convertedTime = DateFormat('hh:mm:ss a').format(userParsedTime);
+    });
+  }
+
+  onUserTimeZoneChange(index) {
+    validLoc = getValidLocation(timezonesList[selectedTzIndex]['utc']);
+    var currentTime = DateTime.now();
+    var tzParsedTime = TZDateTime(
+        validLoc,
+        currentTime.year,
+        currentTime.month,
+        currentTime.day,
+        int.parse(parsedTime.substring(0, 2)),
+        int.parse(parsedTime.substring(2, 4)));
+    userParsedTime = TZDateTime.from(
+        tzParsedTime,
+        getValidLocation(userTzList[index]['utc']));
+    setState(() {
+      selectedUserTzIndex = index;
+      location = validLoc.name;
+      userTz = userTzList[index]['utc'][0];
+      convertedTime = DateFormat('hh:mm:ss a').format(userParsedTime);
+    });
   }
 
   Location getValidLocation(List<String> locations) {
@@ -70,96 +119,259 @@ class _ShowParsedTzState extends State<ShowParsedTz> {
       appBar: AppBar(
         title: const Text('Your time'),
       ),
-      body: Container(
-        padding: const EdgeInsets.all(24),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Time as provided ',
-              style: const TextStyle(fontSize: 22),
-            ),
-            SizedBox(
-              height: 8,
-            ),
-            Text(
-              '$parsedTime',
-              style: const TextStyle(
-                  fontFamily: 'Pixel', fontSize: 44, color: Color(0xff373e9e)),
-            ),
-            SizedBox(
-              height: 8,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                const Text(
-                  'in ',
-                  style: const TextStyle(fontSize: 22),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.all(24),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              timezonesList.length > 1 ? Padding(
+                padding: const EdgeInsets.only(bottom: 32),
+                child: RichText(
+                  text: TextSpan(
+                      children: [
+                        TextSpan(
+                            text: 'Uh oh! ',
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontFamily: 'CabinSketch',
+                                fontSize: 20
+                            )
+                        ),
+                        TextSpan(
+                            text: '$tz ',
+                            style: TextStyle(
+                                fontWeight: FontWeight.w700,
+                                color: Colors.black,
+                                fontFamily: 'CabinSketch',
+                                fontSize: 20
+                            )
+                        ),
+                        TextSpan(
+                            text: 'means more than one timezone\n\nWhat it might stand for?',
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontFamily: 'CabinSketch',
+                                fontSize: 20
+                            )
+                        )
+                      ]
+                  ),
+                  textAlign: TextAlign.center,
                 ),
-                Text(
-                  '$location',
-                  style: const TextStyle(
-                      fontSize: 22,
-                      fontWeight: FontWeight.w900,
-                      color: Colors.black),
-                ),
-                const Text(
-                  ' is',
-                  style: const TextStyle(fontSize: 22),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 24,
-            ),
-            Text(
-              '$convertedTime',
-              style: const TextStyle(
-                  fontFamily: 'Pixel', fontSize: 44, color: Colors.teal),
-            ),
-            SizedBox(
-              height: 24,
-            ),
-            const Text(
-              'in your local timezone',
-              style: const TextStyle(fontSize: 22),
-            ),
-            SizedBox(
-              height: 8,
-            ),
-            Text(
-              '$userTz',
-              style: const TextStyle(
-                  fontFamily: 'Pixel', fontSize: 44, color: Colors.deepPurple),
-            ),
-            SizedBox(
-              height: 32,
-            ),
-            RaisedButton(
-              elevation: 0,
-              onPressed: () {
-                final Event event = Event(
-                    title: 'Event title',
-                    description: 'Event description',
-                    location: 'Event location',
-                    startDate: userParsedTime,
-                    endDate: userParsedTime
-                );
-                Add2Calendar.addEvent2Cal(event);
-              },
-              color: Colors.blueAccent,
-              child: Text(
-                'Add Calendar Event',
-                style: const TextStyle(
-                    color: Colors.white, fontWeight: FontWeight.w900),
+              ): Text(''),
+              timezonesList.length > 1
+                  ? Flexible(
+                child: GridView.builder(
+                    shrinkWrap: true,
+                    gridDelegate:
+                    SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                    ),
+                    itemCount: timezonesList.length,
+                    itemBuilder: (context, position) {
+                      return timeZoneCard(timezonesList[position],
+                          position, selectedTzIndex, onTimezoneChange);
+                    }),
+              )
+                  : Container(
+                width: 0,
+                height: 0,
               ),
-            )
+              timezonesList.length > 1
+                  ? SizedBox(
+                height: 24,
+              )
+                  : SizedBox(
+                height: 0,
+              ),
+              userTzList.length > 1 ? Padding(
+                padding: const EdgeInsets.only(bottom: 32),
+                child: RichText(
+                  text: TextSpan(
+                      children: [
+                        TextSpan(
+                            text: 'and also your timezone ',
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontFamily: 'CabinSketch',
+                                fontSize: 20
+                            )
+                        ),
+                        TextSpan(
+                            text: '$userTzName ',
+                            style: TextStyle(
+                                fontWeight: FontWeight.w700,
+                                color: Colors.black,
+                                fontFamily: 'CabinSketch',
+                                fontSize: 20
+                            )
+                        ),
+                        TextSpan(
+                            text: 'means more than one timezone\n\nPlease select one',
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontFamily: 'CabinSketch',
+                                fontSize: 20
+                            )
+                        )
+                      ]
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ): Text(''),
+              userTzList.length > 1
+                  ? Flexible(
+                child: GridView.builder(
+                    shrinkWrap: true,
+                    gridDelegate:
+                    SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                    ),
+                    itemCount: userTzList.length,
+                    itemBuilder: (context, position) {
+                      return timeZoneCard(userTzList[position],
+                          position, selectedUserTzIndex, onUserTimeZoneChange);
+                    }),
+              )
+                  : Container(
+                width: 0,
+                height: 0,
+              ),
+              userTzList.length > 1
+                  ? SizedBox(
+                height: 24,
+              )
+                  : SizedBox(
+                height: 0,
+              ),
+              const Text(
+                'Time as provided ',
+                style: const TextStyle(fontSize: 22),
+              ),
+              SizedBox(
+                height: 8,
+              ),
+              Text(
+                '$parsedTime',
+                style: const TextStyle(
+                    fontFamily: 'Pixel', fontSize: 44, color: Color(0xff373e9e)),
+              ),
+              SizedBox(
+                height: 8,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  const Text(
+                    'in ',
+                    style: const TextStyle(fontSize: 22),
+                  ),
+                  Text(
+                    '$location',
+                    style: const TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.w900,
+                        color: Colors.black),
+                  ),
+                  const Text(
+                    ' is',
+                    style: const TextStyle(fontSize: 22),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 24,
+              ),
+              Text(
+                '$convertedTime',
+                style: const TextStyle(
+                    fontFamily: 'Pixel', fontSize: 44, color: Colors.teal),
+              ),
+              SizedBox(
+                height: 24,
+              ),
+              const Text(
+                'in your local timezone',
+                style: const TextStyle(fontSize: 22),
+              ),
+              SizedBox(
+                height: 8,
+              ),
+              Text(
+                '$userTz',
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                    fontFamily: 'Pixel', fontSize: 44, color: Colors.deepPurple),
+              ),
+              SizedBox(
+                height: 32,
+              ),
+              RaisedButton(
+                elevation: 0,
+                onPressed: () {
+                  final Event event = Event(
+                      title: 'Event title',
+                      description: 'Event description',
+                      location: 'Event location',
+                      startDate: userParsedTime,
+                      endDate: userParsedTime
+                  );
+                  Add2Calendar.addEvent2Cal(event);
+                },
+                color: Colors.blueAccent,
+                child: Text(
+                  'Add Calendar Event',
+                  style: const TextStyle(
+                      color: Colors.white, fontWeight: FontWeight.w900),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget timeZoneCard(dynamic data, index, selectedIndex, onTimezoneChange) {
+    return InkWell(
+      onTap: () {
+        onTimezoneChange(index);
+      },
+      child: Card(
+        elevation: 4,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16),
+          side: index == selectedIndex
+              ? BorderSide(color: Colors.deepPurple, width: 2)
+              : BorderSide.none,
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: Text(
+                data['value'],
+                style: TextStyle(
+                    color: Theme.of(context).accentColor, fontSize: 22),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(16, 0, 16, 16),
+              child: Text(
+                data['text'],
+                style: TextStyle(color: Colors.grey, fontSize: 16),
+              ),
+            ),
           ],
         ),
       ),
     );
   }
+
+
 }
